830271105554f34e07278d0b0464ca8f47ce0a59
1adamcollins1 <adamcollins31@gmail.com>
2025-11-04 16:39:30 +0000

    git change

--- components/cvSection/skills.vue
+++ components/cvSection/skills.vue
@@ -9,7 +9,7 @@
             <p>SCRUM, Agile, GitLab Boards, Jira, Project Management, Client Communication, Project Presentation</p>
         
             <h3 class="font-bold">Technologies</h3>
-            <p>Docker, Figma, Postman, FileZilla, Siteground, Git CLI, Git Version Control, REST API’s, GitLab management, Fintech</p>
+            <p>Docker, Figma, Postman, FileZilla, Siteground, Git CLI, Github, REST API’s, GitLab management, Fintech</p>
         </div>
     </div>
 </template>

7bd68ccd6b32cd557d3bf7e6328c1d74181e474d
1adamcollins1 <adamcollins31@gmail.com>
2025-11-25 15:05:56 +0000

    update skills

--- components/cvSection/skills.vue
+++ components/cvSection/skills.vue
@@ -6,13 +6,13 @@
         >Skills</h2>
         <div class="w-full overflow-scroll flex-1 pb-4">
             <h3 class="font-bold">Languages</h3>
-            <p>TypeScript, Vue.js, ReactJS, React Native, Nuxt.js, HTML5, JavaScript, CSS, SCSS, SASS, Node.js, WordPress, GraphQL, SQL</p>
+            <p>TypeScript, Vue.js, ReactJS, React Native, Nuxt.js, Next.js, PHP, Node.js HTML5, JavaScript, CSS, SCSS, Tailwind, Bootstrap, Node.js, WordPress, GraphQL, SQL, Pina</p>
         
             <h3 class="font-bold">Development</h3>
-            <p>SCRUM, Agile, GitLab Boards, Jira, Project Management, Client Communication, Project Presentation</p>
+            <p>SCRUM, Agile, Jira, Project Management, Client Communication, Project Presentation, QA Testing, UX/UI Design</p>
         
             <h3 class="font-bold">Technologies</h3>
-            <p>Docker, Figma, Postman, FileZilla, Siteground, Git CLI, Github, REST API’s, GitLab management, Fintech, Pinia, Expo</p>
+            <p>Docker, Figma, Postman, FileZilla, Siteground, Git CLI, Git Version Control, REST API's,  GitLab management, Jira, Fintech, Google Analytics, Firebase, Cloudflare</p>
         </div>
     </div>
 </template>
